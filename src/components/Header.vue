65092ea19970932bd57bcbcab0f6716c97301a29
Franklin P. Ramos <franky88@users.noreply.github.com>
2022-11-23 15:05:52 +0800

    updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
     <nav class="navbar navbar-expand-lg" style="background-color: #ffffff;">
         <div class="container"> 
-            <RouterLink to="/" class="navbar-brand">Franklin Ramos</RouterLink><small class="text-muted">Graphic designer & Motion graphic animator</small>
+            <RouterLink to="/" class="navbar-brand">Franklin Ramos</RouterLink><small class="text-muted">Multimedia Artist</small>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,12 +6,12 @@
   
 
   <div class="row">
-    <div class="col-sm-12 mt-5">
+    <!-- <div class="col-sm-12 mt-5">
       <div class="about-me">
         <div class="profile-card">
           <div class="profile-body-content">
             <div class="profile-image">
-              <img src="" alt="">
+              <img src="../assets/profile.jpg" alt="profile">
             </div>
             <h2 style="text-align: center;">Franklin <br> Ramos</h2>
             <small style="text-align: center;" class="text-muted">Graphic designer & <br> Motion graphic animator</small>
@@ -36,6 +36,36 @@
           
         </div>
       </div>
+    </div> -->
+    <div class="col-sm-6 d-flex justify-content-end">
+      <div class="profile-card">
+        <div class="profile-body-content">
+          <div class="profile-image">
+            <img src="../assets/profile.jpg" alt="profile">
+          </div>
+          <h2 style="text-align: center;">Franklin <br> Ramos</h2>
+          <small style="text-align: center;" class="text-muted">Graphic designer & <br> Motion graphic animator</small>
+        </div>
+        <div class="profile-footer">
+          <a href=""><span class="fa-brands fa-linkedin"></span></a>
+          <a href=""><i class="fa-brands fa-instagram"></i></a>
+          <a href=""><i class="fa-brands fa-behance"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 d-flex justify-content-start">
+      <div class="profile-content">
+        <h1 class="header-title">
+          HELLO!
+        </h1>
+        <small class="self-intro">
+          I am Franklin P. Ramos. I am a graphic artist and designer in the Philippines. I’ve been in the industry since 2009. I started as a trainer for 2D Animation classes and aspiring graphic artists and designers. I have my National Certificate (NC) in 2D Animation NC – III and Visual Graphic Design (VGD–NC–III). My previous employment involved so much designing related works like logo design, promo banners, ad campaigns, product packaging, flyers, brochures, infographics, Image editing, Video editing, and the like.
+        </small>
+        <div class="mt-5 action-btn">
+          <a href="" class="btn btn-outline-info">Download CV</a>
+          <a href="" class="btn btn-outline-info">Portfolio</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -44,16 +74,18 @@
 .about-me {
   /* margin-top: 300px; */
   /* margin: auto; */
-  vertical-align: middle;
+  /* vertical-align: middle; */
+  height: auto;
   display: flex;
-  flex-direction: row;
+  /* flex-direction: row; */
   justify-content: center;
   align-items: center;
   gap: 3rem;
+  flex-wrap: wrap;
 }
 .profile-card {
   /* position: absolute; */
-  width: 30%;
+  width: 80%;
   height: auto;
   background-color: #fff;
   border-radius: 7px;
@@ -87,7 +119,7 @@
   font-size: 16pt;
 }
 .profile-content {
-  width: 30%;
+  width: 80%;
 }
 .header-title {
   font-size: 80px;
@@ -100,5 +132,10 @@
 .action-btn {
   display: flex;
   gap: 2rem;
+}
+img {
+  width: 100%;
+  height: auto;
+  border-radius: 90px;
 }
 </style>
